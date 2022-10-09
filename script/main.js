e7767e5ebce35a68dad07b307c4ce51363b2f3ca
fdy7n <fuadynur7@gmail.com>
2022-10-09 17:55:37 +0700

    All updated
    Siap deploy

--- script/main.js
+++ script/main.js
@@ -72,6 +72,20 @@ const animationTimeline = () => {
       },
       "+=2"
     )
+    .from(".three2", 0.7, {
+      opacity: 0,
+      y: 10
+      // scale: 0.7
+    })
+    .to(
+      ".three2",
+      0.7,
+      {
+        opacity: 0,
+        y: 10
+      },
+      "+=2"
+    )
     .from(".four", 0.7, {
       scale: 0.2,
       opacity: 0
